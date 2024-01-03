512025eb60feb7f3a04582700b8654e0e4841703
TusharTyagi-63 <133961306+TusharTyagi-63@users.noreply.github.com>
2024-01-03 10:25:50 +0530

    Bus Booking App

---
+++ BusBookingApp/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Bus Booking App");
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setBounds(400,100,700,600);
+    }
+}
