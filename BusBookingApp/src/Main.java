7c26a38a71b7663847cf4f9eed5ed08c88fde2e2
TusharTyagi-63 <133961306+TusharTyagi-63@users.noreply.github.com>
2024-01-05 12:26:10 +0530

    completed

--- BusBookingApp/src/Main.java
+++ BusBookingApp/src/Main.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         JFrame busBookingApp = new JFrame("Bus Booking App");
-        busBookingApp.setVisible(true);
-        busBookingApp.setBounds(400, 100, 700, 600);
+
 
 
         JLabel name = new JLabel("Name");
@@ -23,15 +22,20 @@ public class Main {
             jLabel.setVisible(true);
             jLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         }
-        for (int i = 10; i <= 290; i++) {
-            for (JLabel jLabel : Arrays.asList(name, email,
-                    mobile, address, source, destination, payment, gender)) {
-                jLabel.setBounds(10, i, 150, 30);
-                i = i + 40;
-            }
-        }
-        for (JLabel jLabel : Arrays.asList(name, email,
-                mobile, address, source, destination, payment, gender)) {
+
+        name.setBounds(10,10,150,30);
+        email.setBounds(10,50,150,30);
+        mobile.setBounds(10,90,150,30);
+        address.setBounds(10,130,150,30);
+        source.setBounds(10,170,150,30);
+        destination.setBounds(10,210,150,30);
+        payment.setBounds(10,250,150,30);
+        gender.setBounds(10,290,150,30);
+
+
+
+        for ( JLabel jLabel : Arrays.asList(name, email,
+                mobile, address, source, destination, payment, gender)){
             busBookingApp.add(jLabel);
         }
 
@@ -49,15 +53,20 @@ public class Main {
             jTextField.setVisible(true);
             jTextField.setFont(new Font("Arial", Font.ITALIC, 20));
         }
-        for (int i = 10; i <= 290; i++) {
-            for (JTextField jTextField : Arrays.asList(name_tf, email_tf, mobile_tf, address_tf,
-                    source_tf, destination_tf, payment_tf, gender_tf)) {
-                jTextField.setBounds(130, i, 150, 30);
-                i = i + 40;
-            }
-        }
-        for (JTextField jTextField : Arrays.asList(name_tf, email_tf, mobile_tf, address_tf,
-                source_tf, destination_tf, payment_tf, gender_tf)) {
+        
+        name_tf.setBounds(130,10,150,30);
+        email_tf.setBounds(130,50,150,30);
+        mobile_tf.setBounds(130,90,150,30);
+        address_tf.setBounds(130,130,150,30);
+        source_tf.setBounds(130,170,150,30);
+        destination_tf.setBounds(130,210,150,30);
+        payment_tf.setBounds(130,250,150,30);
+        gender_tf.setBounds(130,290,150,30);
+
+
+
+        for ( JTextField jTextField : Arrays.asList(name_tf, email_tf, mobile_tf, address_tf,
+                source_tf, destination_tf, payment_tf, gender_tf)){
             busBookingApp.add(jTextField);
         }
 
@@ -66,23 +75,24 @@ public class Main {
         JButton cancelBtn = new JButton("Cancel");
         JButton exitBtn = new JButton("Exit");
 
-        for (JButton jButton : Arrays.asList(book_bt, cancelBtn, exitBtn)) {
+        for (JButton jButton : Arrays.asList(book_bt,cancelBtn,exitBtn)) {
             jButton.setVisible(true);
             jButton.setFont(new Font("Arial", Font.PLAIN, 20));
         }
         for (int i = 10; i <= 200; i++) {
-            for (JButton jButton : Arrays.asList(book_bt, cancelBtn, exitBtn)) {
-                jButton.setBounds(i, 330, 150, 30);
+            for (JButton jButton : Arrays.asList(book_bt,cancelBtn,exitBtn)) {
+                jButton.setBounds(i, 350, 150, 30);
                 i = i + 200;
             }
         }
-        for (JButton jButton : Arrays.asList(book_bt, cancelBtn, exitBtn)) {
+        for ( JButton jButton : Arrays.asList(book_bt,cancelBtn,exitBtn)){
             busBookingApp.add(jButton);
         }
-
 
         busBookingApp.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         busBookingApp.setLayout(null);
         busBookingApp.setResizable(false);
+        busBookingApp.setVisible(true);
+        busBookingApp.setBounds(400, 100, 700, 600);
     }
 }
